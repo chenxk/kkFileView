88963c6720b240867f8b5befeaf3f944c538369b
charles.chen <chenxunkun@meorient.com>
2020-08-15 16:30:59 +0800

    增加跨域处理

--- jodconverter-web/src/main/java/cn/keking/web/filter/FilterConfiguration.java
+++ jodconverter-web/src/main/java/cn/keking/web/filter/FilterConfiguration.java
@@ -4,7 +4,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.HashSet;
